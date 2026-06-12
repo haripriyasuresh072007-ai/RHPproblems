a7bab716bde5cdb7033b93e0d794754f040d94bd
Haripriya S <haripriyasuresh072007@gmail.com>
2026-06-12 11:57:57 +0530

    Update dpmaxvalcol.cpp

--- dpmaxvalcol.cpp
+++ dpmaxvalcol.cpp
@@ -1,46 +1,35 @@
-package RHPproblems;
-
-import java.util.*;
-public class dpmaxval {
-    public static void main(String[] args) {
-        int R, C;
-        long[][] mat;
-        try (Scanner sc = new Scanner(System.in)) {
-            R = sc.nextInt();
-            C = sc.nextInt();
-
-            mat = new long[R][C];
-            for (int i = 0; i < R; i++)
-                for (int j = 0; j < C; j++)
-                    mat[i][j] = sc.nextLong();
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+int main(){
+    int r,c;
+    cin>>r>>c;
+    vector<vector<int>> mat(r,vector<int>(c));
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>mat[i][j];
         }
-
-        long[][] dp = new long[R][C];
-
-        System.arraycopy(mat[0], 0, dp[0], 0, C); 
-
-        for (int i = 1; i < R; i++) {
-            long[] fsmax = getFSMax(dp, i - 1, C);
-            for (int j = 0; j < C; j++) {
-                dp[i][j] = mat[i][j] + (dp[i-1][j] == fsmax[0] ? fsmax[1] : fsmax[0]);
-            }
-        }
-
-        System.out.println(getFSMax(dp, R - 1, C)[0]);
     }
-
-    private static long[] getFSMax(long[][] dp, int row, int col) {
-        long fmax = Long.MIN_VALUE;
-        long smax = Long.MIN_VALUE;
-        for (int j = 0; j < col; j++) {
-            if (dp[row][j] > fmax) {
-                smax = fmax;
-                fmax = dp[row][j];
-            } else if (dp[row][j] > smax) {
-                smax = dp[row][j];
-            }
-        }
-        return new long[]{fmax, smax};
+    vector<vector<int>> dp(r,vector<int>(c,0));
+    for(int i=0;i<r;i++){
+        dp[i][0]=mat[i][0];
     }
+    for(int j=1;j<c;j++){
+        for(int i=0;i<r;i++){
+            int best=INT_MIN;
+            for(int pr=0;pr<r;pr++){
+                if(pr==r) continue;
+                if(dp[pr][j-1]>best){
+                    best=dp[pr][j-1];
+                }
+            }
+            dp[i][j]=mat[i][j]+best;
+        }
+    }
+    int result=INT_MIN;
+    for(int i=0;i<r;i++){
+        result=max(result,dp[i][c-1]);
+    }
+    cout<<result<<endl;
 }
-
