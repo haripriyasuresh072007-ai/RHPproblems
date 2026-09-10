f274d60cb40a50985b3940d3801ff3fe0c046563
Haripriya S <haripriyasuresh072007@gmail.com>
2026-09-10 11:25:24 +0530

    Refactor longest common substring implementation

--- longestcommonsubstring.java
+++ longestcommonsubstring.java
@@ -10,32 +10,24 @@ public class longestcommonsubstring {
         int m = s1.length();
         int n = s2.length();
         
-        // DP table to store tracking lengths
         int[][] dp = new int[m + 1][n + 1];
         
         int maxLength = 0;
-        int endIndexS1 = 0; // Tracks ending position in s1 to extract the substring
-
-        // Fill the DP table
+        int endIndexS1 = 0; 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                // If characters match, extend the previous diagonal chain length
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    
-                    // Update global max and capture ending position
                     if (dp[i][j] > maxLength) {
                         maxLength = dp[i][j];
                         endIndexS1 = i; 
                     }
                 } else {
-                    // Mismatch breaks continuity
                     dp[i][j] = 0;
                 }
             }
         }
 
-        // Extract and return the actual substring using recorded indices
         return s1.substring(endIndexS1 - maxLength, endIndexS1);
     }
 
